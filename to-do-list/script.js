5303b4b7078fd885c64459f79c543142ac66fa75
Amirmasud <amirmasudpoolaee@yahoo.com>
2023-01-06 12:48:25 +0330

    add localstorage to todo list

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -4,7 +4,14 @@ let clearBtn = document.querySelector(".clear-btn");
 let list = document.querySelector("ul");
 let alert = document.querySelector(".alert")
 
+let itemsList = JSON.parse(localStorage.getItem("itemsList")) || [];
 
+if (itemsList.length) {
+    list.innerHTML = "";
+    itemsList.forEach(item => {
+        list.appendChild(createLi(item.value, item.condition));
+    })
+}
 
 addBtn.addEventListener("click", e => {
     if (!input.value) {
@@ -13,23 +20,41 @@ addBtn.addEventListener("click", e => {
             alert.style.display = "none";
         }, 3000);
     } else {
-        list.appendChild(createLi(input.value))
+        let listItemsInfo = new ListItemsInfo(input.value, false)
+        itemsList.push(listItemsInfo);
+        localStorage.setItem("itemsList", JSON.stringify(itemsList));
+        list.innerHTML = "";
+        itemsList.forEach(item => {
+            list.appendChild(createLi(item.value, item.condition));
+        })
         input.value = "";
     }
 
 })
 
+
+
 list.addEventListener("click", e => {
 
     if (e.target.classList.contains("fa-times-circle")) {
+        let index = Array.from(list.children).findIndex(item => item == e.target.parentNode);
+        itemsList.splice(index, 1);
         e.target.parentNode.remove();
     } else if (e.target.classList.contains("fa-check-circle")) {
         e.target.classList.toggle("done-icon");
-        e.target.parentNode.firstChild.classList.toggle("done-item")
+        if (e.target.classList.contains("done-icon")) {
+            let index = Array.from(list.children).findIndex(item => item == e.target.parentNode);
+            itemsList[index].condition = true;
+        }
+        e.target.parentNode.firstChild.classList.toggle("done-item");
     } else if (e.target.classList.contains("fa-edit")) {
         input.value = e.target.parentNode.firstChild.innerText;
         e.target.parentNode.remove();
+        let index = Array.from(list.children).findIndex(item => item == e.target.parentNode);
+        itemsList.splice(index, 1);
     }
+
+    localStorage.setItem("itemsList", JSON.stringify(itemsList));
 })
 
 
@@ -38,11 +63,13 @@ clearBtn.addEventListener("click", () => {
     Array.from(list.children).forEach(child => {
         child.remove();
     })
+    itemsList = [];
+    localStorage.setItem("itemsList", JSON.stringify(itemsList));
 })
 
 
 
-function createLi(input) {
+function createLi(input, condition) {
 
     let li = document.createElement("li");
     let span = document.createElement("span");
@@ -55,10 +82,21 @@ function createLi(input) {
     let remove = document.createElement("i");
     remove.classList.add("fa", "fa-times-circle");
 
+    if (condition) {
+        check.classList.add("done-icon");
+        span.classList.add("done-item");
+    }
+
+
     li.appendChild(span);
     li.appendChild(check);
     li.appendChild(edit);
     li.appendChild(remove);
 
     return li;
+}
+
+function ListItemsInfo(value, condition) {
+    this.value = value;
+    this.condition = condition;
 }
